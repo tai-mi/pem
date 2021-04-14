e951d026289ba0c4ab5dcd0399a496bf535ca8e8
tai-mi <tai.michaels01@gmail.com>
2021-04-14 15:05:14 -0400

    update dat with integration for seurat

---
+++ markers2.r
@@ -0,0 +1,11 @@
+# libraries ---------------------------------------------------------------
+
+library(magrittr)
+library(tidyverse)
+
+rna <- readRDS('data_sc/scaled_rna2.rds')
+sct <- readRDS('data_sc/scaled_sct2.rds')
+
+
+# stuff -------------------------------------------------------------------
+
